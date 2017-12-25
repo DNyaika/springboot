ed5b98b9ad098364b1c3982f3277dbb3b617c22f
DESKTOP-5LKLR5J\DNyaika <davie2086@gmail.com>
2017-12-25 14:29:52 -0600

    Spring rest capabilities

--- src/main/java/com/example/demo/SpringMvcController.java
+++ src/main/java/com/example/demo/SpringMvcController.java
@@ -1,18 +1,36 @@
 package com.example.demo;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.domain.Player;
+import com.example.demo.domain.Team;
+
 @Controller
 public class SpringMvcController {
-	@RequestMapping("/hi/{name}")
-	public String hiThere(Map model,@PathVariable String name) {
-		model.put("name", name);
-		return "hello";
+	
+	private Team team;
+	
+	@PostConstruct
+	public void init() {
+		Set<Player> players = new HashSet<>();
+		players.add(new Player("Charlie Brown", "Pitcher"));
+		players.add(new Player("Snoopy", "Shortstop"));
+		team = new Team("Carlifornia", "Peanuts", players);
+	}
+	
+	
+	@RequestMapping("/hi")
+	public @ResponseBody Team hiThere() {
+		return team;
 	}
 
 }
